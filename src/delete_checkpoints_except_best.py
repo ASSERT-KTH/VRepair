b672e9b266cf573e15b9147afa7d365fe8259636
Zimin <chen.zimin@hotmail.com>
2021-10-04 17:30:09 +0200

    change delete script

--- src/delete_checkpoints_except_best.py
+++ src/delete_checkpoints_except_best.py
@@ -14,8 +14,8 @@ def main():
         all_model_checkpoints = list(log_dir.rglob('*.pt'))
         all_model_checkpoints.sort(key=lambda x: int(x.stem.split('_')[-1]))
         best_model_step = -1
-        with open(log_file) as log_file:
-            for line in log_file:
+        with open(log_file) as f:
+            for line in f:
                 if 'Best model found at step' in line:
                     best_model_step = line.strip().split(' ')[-1]
                     break
@@ -23,6 +23,8 @@ def main():
             best_model_path = all_model_checkpoints[-1]
         else:
             best_model_path = log_dir / ('model_step_' + best_model_step + '.pt')
+
+        assert best_model_path in all_model_checkpoints
 
         for checkpoint_file in log_dir.rglob('*.pt'):
             if checkpoint_file != best_model_path:
