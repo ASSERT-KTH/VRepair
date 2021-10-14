845046a0217327aaad2f4d8821d734cdf41b3e1c
Zimin <chen.zimin@hotmail.com>
2021-10-14 15:58:26 +0200

    Update rq4 script

--- src/rq4_compare.sh
+++ src/rq4_compare.sh
@@ -13,7 +13,7 @@ for sweep in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19;do
   lr=$(grep 'learning_rate:' /cephyr/NOBACKUP/groups/snic2021-23-24/vulnerability_repair/major_revision_2021_09/only_first_line_context3_models/${sweep}_parameter_sweep/train_config.yml)
   rs=$(grep 'rnn_size:' /cephyr/NOBACKUP/groups/snic2021-23-24/vulnerability_repair/major_revision_2021_09/only_first_line_context3_models/${sweep}_parameter_sweep/train_config.yml)
   echo "${lr}, ${rs}"
-  python3 compare.py --tgt /cephyr/NOBACKUP/groups/snic2021-23-24/vulnerability_repair/major_revision_2021_09/only_first_line_context3_models/big_vul_year_test_data/big_vul_year_fine_tune_test.tgt.txt --src /cephyr/NOBACKUP/groups/snic2021-23-24/vulnerability_repair/major_revision_2021_09/only_first_line_big_vul_year_fine_tune_context3_models/${sweep}_parameter_sweep/big_vul_year_fine_tune/big_vul_year_fine_tune_test.src.txt.predictions
+  python3 compare.py --tgt /cephyr/NOBACKUP/groups/snic2021-23-24/vulnerability_repair/major_revision_2021_09/only_first_line_context3_models/big_vul_year_test_data/big_vul_year_fine_tune_test.tgt.txt --src /cephyr/NOBACKUP/groups/snic2021-23-24/vulnerability_repair/major_revision_2021_09/only_first_line_context3_models/${sweep}_parameter_sweep/big_vul_year_fine_tune/big_vul_year_fine_tune_test.src.txt.predictions
   echo
 done
 echo
@@ -31,7 +31,7 @@ for sweep in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19;do
   lr=$(grep 'learning_rate:' /cephyr/NOBACKUP/groups/snic2021-23-24/vulnerability_repair/major_revision_2021_09/only_first_line_context3_models/${sweep}_parameter_sweep/train_config.yml)
   rs=$(grep 'rnn_size:' /cephyr/NOBACKUP/groups/snic2021-23-24/vulnerability_repair/major_revision_2021_09/only_first_line_context3_models/${sweep}_parameter_sweep/train_config.yml)
   echo "${lr}, ${rs}"
-  python3 compare.py --tgt /cephyr/NOBACKUP/groups/snic2021-23-24/vulnerability_repair/major_revision_2021_09/only_first_line_context3_models/cve_fixes_year_test_data/cve_fixes_year_fine_tune_test.tgt.txt --src /cephyr/NOBACKUP/groups/snic2021-23-24/vulnerability_repair/major_revision_2021_09/only_first_line_cve_fixes_year_fine_tune_context3_models/${sweep}_parameter_sweep/cve_fixes_year_fine_tune/cve_fixes_year_fine_tune_test.src.txt.predictions
+  python3 compare.py --tgt /cephyr/NOBACKUP/groups/snic2021-23-24/vulnerability_repair/major_revision_2021_09/only_first_line_context3_models/cve_fixes_year_test_data/cve_fixes_year_fine_tune_test.tgt.txt --src /cephyr/NOBACKUP/groups/snic2021-23-24/vulnerability_repair/major_revision_2021_09/only_first_line_context3_models/${sweep}_parameter_sweep/cve_fixes_year_fine_tune/cve_fixes_year_fine_tune_test.src.txt.predictions
   echo
 done
 echo
